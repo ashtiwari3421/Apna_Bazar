572b189d8564a89b93d7b753dc3e89b2691ab1cd
Ashutosh Tiwari <ashtiwari3421@gmail.com>
2022-02-22 22:37:59 +0530

    first commit
    first commit

---
+++ app/src/main/java/com/example/apna_bazar/Post_donate.java
@@ -0,0 +1,13 @@
+package com.example.apna_bazar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Post_donate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_donate);
+    }
+}
