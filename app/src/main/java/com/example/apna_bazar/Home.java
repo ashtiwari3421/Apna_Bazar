972fda562578e9936a5e0cdc028e54fec8b15b05
DivyanshNegi07 <divyanshnegi884@gmail.com>
2022-02-23 12:11:44 +0530

    signout

--- app/src/main/java/com/example/apna_bazar/Home.java
+++ app/src/main/java/com/example/apna_bazar/Home.java
@@ -18,7 +18,7 @@ public class Home extends AppCompatActivity {
 
     public void logout(View view){
         FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getApplicationContext(),Login.class));              
+        startActivity(new Intent(getApplicationContext(),Login.class));
         finish();
     }
 }
