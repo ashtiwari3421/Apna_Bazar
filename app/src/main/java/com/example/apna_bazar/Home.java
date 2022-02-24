4705b2210e311394c1ecfadb011cb56921bad344
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2022-02-25 01:48:30 +0530

    home buttons done

--- app/src/main/java/com/example/apna_bazar/Home.java
+++ app/src/main/java/com/example/apna_bazar/Home.java
@@ -7,10 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity {
+    ImageButton mCreateBtn;
+    ImageButton nCreateBtn;
+    ImageButton oCreateBtn;
+    ImageButton pCreateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,40 @@ public class Home extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
+        mCreateBtn = findViewById(R.id.buyButton);
+        nCreateBtn = findViewById(R.id.sellbutton);
+        oCreateBtn = findViewById(R.id.donate);
+        pCreateBtn = findViewById(R.id.donations);
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Buylist.class ));
+            }
+        });
+        nCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Post_sell.class ));
+            }
+        });
+        oCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Post_donate.class ));
+            }
+        });
+        pCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Donatelist.class ));
+            }
+        });
+
+
     }
+
+
 
     public void logout(View view){
         FirebaseAuth.getInstance().signOut();
