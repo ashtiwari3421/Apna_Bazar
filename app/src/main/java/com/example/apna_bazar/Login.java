041c8b968609f691d9dd760b722ac23371c5c442
DivyanshNegi07 <divyanshnegi884@gmail.com>
2022-02-25 21:03:38 +0530

    log in text corrected

--- app/src/main/java/com/example/apna_bazar/Login.java
+++ app/src/main/java/com/example/apna_bazar/Login.java
@@ -70,7 +70,7 @@ public class Login extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
                         if (task.isSuccessful()) {
-                            Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Login.this, "Log in Successfully", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getApplicationContext(), Home.class));
                         } else {
                             Toast.makeText(Login.this, " Error  " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
