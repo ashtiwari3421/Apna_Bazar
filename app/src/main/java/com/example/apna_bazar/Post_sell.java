aacada7aacabcbcc0e81785bf7e35f1d8115a130
Ashutosh Tiwari <ashtiwari3421@gmail.com>
2022-02-26 14:11:49 +0530

    post sell

--- app/src/main/java/com/example/apna_bazar/Post_sell.java
+++ app/src/main/java/com/example/apna_bazar/Post_sell.java
@@ -11,6 +11,8 @@ public class Post_sell extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_post_sell);
 
         setContentView(R.layout.activity_post_sell);
     }
